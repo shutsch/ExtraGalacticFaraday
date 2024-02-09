b09866de9e9015165828506293c25e7acd7e6dc9
Andrea <andreacabriolu@gmail.com>
2024-02-09 01:37:53 +0100

    inserted adder operator for adding noise

--- src/model_library/noise_models/egal_adding_noise.py
+++ src/model_library/noise_models/egal_adding_noise.py
@@ -7,24 +7,14 @@ from ..Model import Model
 class EgalAddingNoise(Model):
 
     def __init__(self, target_domain, noise_params, name=''):
-        if isinstance(noise_params['egal_var'], ift.Field):
-            self.egal_var = ift.makeOp(
-                noise_params['egal_var'], 
-                 sampling_dtype=float)
-            # self.egal_var = ift.makeOp(noise_params['egal_var'], dom=noise_params['egal_var'].domain, sampling_dtype=float)
-        else:
-            self.egal_var = ift.makeOp(
-                ift.Field(self.target_domain, noise_params['egal_var']), 
-                 sampling_dtype=float)
-            # self.egal_var = ift.makeOp(ift.Field(self.target_domain, noise_params['egal_var']), dom=noise_params['egal_var'].domain, sampling_dtype=float)
-
+        self.egal_var = noise_params['egal_var']
         self.emodel = noise_params['emodel']
+
         self.name = name if name == '' else '_' + name
 
         super().__init__(target_domain)
 
     def set_model(self):
-        s2 = self.emodel
-       
-        self._model = self.egal_var + s2
+        egal_op = ift.Adder(self.egal_var)
+        self._model = egal_op @ self.emodel
         
