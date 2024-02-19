453efa2adcd8656ce3ddd61a3fb6df923ae260d0
vvacca <valentina.vacca@inaf.it>
2024-02-19 09:13:58 +0100

    some typos have corrected and other small modifications made

--- src/model_library/extra_galactic_models/demo_extra_gal.py
+++ src/model_library/extra_galactic_models/demo_extra_gal.py
@@ -3,6 +3,8 @@ from ..Model import Model
 import libs as Egf
 import numpy as np
 import scipy as sp
+from astropy.cosmology import FlatLambdaCDM
+import math as m
 
 class ExtraGalDemoModel(Model):
     def __init__(self, target_domain, args):
@@ -16,23 +18,31 @@ class ExtraGalDemoModel(Model):
     def set_model(self):
 
         #new formula -> 
-        # Rm^2 = (L/L0)^Xlum * sigma_int_0^2/(1+z)^4 + D/D0 * sigma_env_0^2
+        # sigmaRm^2 = (L/L0)^Xlum * sigma_int_0^2/(1+z)^4 + D/D0 * sigma_env_0^2
       
         chi_lum = ift.FieldAdapter(self.target_domain, 'chi_lum')
         chi_red = ift.FieldAdapter(self.target_domain, 'chi_red')
         chi_int_0 = ift.FieldAdapter(self.target_domain, 'chi_int_0')
         chi_env_0 = ift.FieldAdapter(self.target_domain, 'chi_env_0')
 
-
-        L0 = Egf.const['L0']
+   
+        light_speed =  Egf.const['c']
+        h =  Egf.const['Planck']['h']
+        Wm = Egf.const['Planck']['Wm']
+        Wc = Egf.const['Planck']['Wc']
+        Wl = Egf.const['Planck']['Wl']
+        H0 = 100 * h
+        
+        cosmo = FlatLambdaCDM(H0=H0, Om0=Wm)  
+        L0 = float(Egf.const['L0'])
         D0 = Egf.const['D0']
-  
-          
+        Dl=cosmo.luminosity_distance(self.z)
 
 
         multiply_z = ift.makeOp(ift.Field(self.target_domain, 1./(1+self.z)**4),sampling_dtype=float)
 
-        multiply_L = ift.makeOp(ift.Field(self.target_domain, np.log(self.L/L0)),sampling_dtype=float)
+        #multiply_L = ift.makeOp(ift.Field(self.target_domain, np.log(self.L/L0)),sampling_dtype=float)
+        multiply_L = ift.makeOp(ift.Field(self.target_domain, np.log(self.L*4*m.pi*Dl**2/L0)),sampling_dtype=float)
 
         norm=(multiply_L @ chi_lum).exp()
 
@@ -40,14 +50,7 @@ class ExtraGalDemoModel(Model):
        
         fact1 = norm * term
 
-   
 
-        h =  Egf.const['Planck']['h']
-        Wm = Egf.const['Planck']['Wm']
-        Wc = Egf.const['Planck']['Wc']
-        Wl = Egf.const['Planck']['Wl']
-        H0 = 100 * h
-        
 
        
         nz = 100  # number of redshift bins
@@ -67,8 +70,9 @@ class ExtraGalDemoModel(Model):
         add_4 = ift.Adder(ift.full(full_domain, 4))
         multiply_1pz = ift.makeOp(z_grid.log(), sampling_dtype=float)
         fact3 = (multiply_1pz @ add_4 @ expander @ chi_red).exp()  # expander maps chi_red on the full domain
-        fact4 = ift.makeOp((3./(H0*(Wm*z_grid**3+Wc*z_grid**2 +Wl)**0.5)*1/D0),sampling_dtype=float)
-        
+        #fact4 = ift.makeOp((3./(H0*(Wm*z_grid**3+Wc*z_grid**2 +Wl)**0.5)*1/D0),sampling_dtype=float)
+        fact4 = ift.makeOp((light_speed/(H0*(Wm*z_grid**3+Wc*z_grid**2 +Wl)**0.5)*1/D0),sampling_dtype=float)
+
         fact5 = fact4 @ fact3
 
         z_weights = ift.makeOp(ift.Field(self.target_domain, self.z / nz),sampling_dtype=float) # these are the z_weights to rescale the integral accordingly
@@ -76,7 +80,7 @@ class ExtraGalDemoModel(Model):
 
         
         sigmaRm2 = fact1 + fact6
-
+        #sigmaRm2 = fact6
         #in case we are interested in the RM not only in its sigma, we should output the operator that does the sampling
         #from a Gaussian with this sigma. This should be possible with the following lines
         #sigmaRm=sigmaRm2.sqrt()
