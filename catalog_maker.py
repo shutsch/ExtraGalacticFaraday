72630dcf1999c25e43d553a72716405f063e5e23
vvacca <valentina.vacca@inaf.it>
2025-03-24 20:20:46 +0100

    Revised mock delta_rm for noise inference.

--- catalog_maker.py
+++ catalog_maker.py
@@ -122,25 +122,37 @@ class CatalogMaker():
         print('Gal Noise std =', noise_gal)
 
         rm_data=np.array(eg_gal_data.val)
-
+        delta_rm_list=[]
         if self.params['params_mock_cat.maker_params.npi']==1:
             b_indices=np.where(np.isnan(data['z_best']))[0]
             np.random.seed(seed=self.params['params_mock_cat.maker_params.seed'])
             npi_indices=np.unique(np.random.choice(b_indices, size=self.params['params_mock_cat.maker_params.npi_los']))
             print(npi_indices)
             print(npi_indices.size)
-            
-            mask=np.full(len(rm_data),False,dtype=bool)
-            mask[npi_indices]=True
-            rm_data[mask] += 14000*np.exp(-np.abs(eg_b[npi_indices]))
-            np.random.seed(seed=self.params['params_mock_cat.maker_params.seed'])
-            delta_rm=np.random.normal(1.4, 0.5,len(rm_data[~mask].size))
-            rm_data[~mask] += delta_rm
+            for item in b_indices:
+                if item in npi_indices:
+                    if np.abs(eg_b[item])>=10.0:
+                        delta_rm=np.sqrt(2.5)
+                        rm_data[item] += delta_rm
+                        delta_rm_list.append(delta_rm)
+                    if np.abs(eg_b[item])>=0.0 and np.abs(eg_b[item])<10.0:
+                        delta_rm=np.sqrt(4.0e6*np.exp(-(eg_b[item])**2/8.0)/np.sqrt(8.0*np.pi)+2.5)
+                        rm_data[item] += delta_rm
+                        delta_rm_list.append(delta_rm)
+                    if np.abs(eg_b[item])>-10.0 and np.abs(eg_b[item])<0.0:
+                        delta_rm=np.sqrt(4.0e6*np.exp(-(eg_b[item])**2/8.0)/np.sqrt(8.0*np.pi)+2.5)
+                        rm_data[item] += -delta_rm
+                        delta_rm_list.append(delta_rm)
+                #else:
+                #    np.random.seed(seed=self.params['params_mock_cat.maker_params.seed'])
+                #    delta_rm=np.random.normal(0, 90-np.abs(eg_b[item]))
+                #    rm_data[item] += delta_rm
+                #    eta_list.append(delta_rm)
 
-           # y = rm_data
-           # for i in (npi_indices):
-           #     rm_data[i]+=14000*np.exp(-eg_b[i])
 
+        delta_rm_array=np.array(delta_rm_list)
+        plt.scatter(np.abs(eg_b[npi_indices]), delta_rm_array)
+        plt.savefig('Delta_rm.png', bbox_inches='tight')
 
         N_eg = ift.ScalingOperator(ift.UnstructuredDomain(lerm), noise_eg, np.float64)
         N_gal = ift.ScalingOperator(ift.UnstructuredDomain(ltheta-lerm), noise_gal, np.float64)
