0d7cd739142d466081a6a49f990d6ce17ea20466
vvacca <valentina.vacca@inaf.it>
2026-02-06 22:56:51 +0100

    Correction to catalog name.

--- src/helper_functions/mock/file_writer.py
+++ src/helper_functions/mock/file_writer.py
@@ -10,7 +10,7 @@ def write_to_file(params, dest_data):
     if params['params_mock_cat.maker_params.surveys.make_survey1']:
         catalog_name=params['file_params.cat_path']+params['params_mock_cat.maker_params.surveys.name1']+'_catalog'+'.fits'
     else:
-        catalog_name=params['file_params.cat_path']+params['params_mock_cat.maker_params.base_catalog']
+        catalog_name=params['file_params.cat_path']+params['params_mock_cat.maker_params.base_catalog']+'.fits'
     
     try:
         hdu= fits.open(catalog_name)
@@ -19,10 +19,11 @@ def write_to_file(params, dest_data):
         hdu[1].data['rm_err'][np.where(hdu[1].data['type']!='Pulsar')] =  dest_data['rm_err']
         hdu[1].data['z_best'][np.where(hdu[1].data['type']!='Pulsar')] =  dest_data['z_best']
         hdu[1].data['stokesI'][np.where(hdu[1].data['type']!='Pulsar')] =  dest_data['stokesI']
-        hdu.writeto(catalog_name, overwrite=True)
+        new_catalog_name= params['file_params.cat_path']+params['params_mock_cat.maker_params.base_catalog']+'_mock.fits'
+        hdu.writeto(new_catalog_name, overwrite=True)
         hdu.close()
         print("Mock catalog written to file successfully.")
-        return catalog_name
+        return new_catalog_name
     
     except Exception as e:
         return "Error in writing mock catalog to file: ", e
